6f853888ac6699d670321289987e8642309f2d93
Katie <katiegyger@my.unt.edu>
2016-09-24 12:03:44 -0500

    Associate Listings with users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160924151401) do
+ActiveRecord::Schema.define(version: 20160924155400) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160924151401) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "user_id"
   end
 
   create_table "models", force: :cascade do |t|
