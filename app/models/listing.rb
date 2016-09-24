a7dbe93b16adb0bdb7f32c0dafc352ce86a83eac
Katie <katiegyger@my.unt.edu>
2016-09-24 10:51:02 -0500

    Add validations for Users and Listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,8 @@
 class Listing < ApplicationRecord
 	has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "images.png"
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
+validates :name, :description, :price, presence: true
+validates :price, numericality: {greter_than: 0}
+validates_attachment_presence :image
 end
